6b7504e173023cf7bf16d937b6c91037c6042c37
LZDXN <47139542+LZDXN@users.noreply.github.com>
2023-11-28 01:45:32 -0600

    added navbar and footer

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import VideoDisplay from "./components/VideoDisplay";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <VideoDisplay />
+      <Navbar />
+      <main>
+        <VideoDisplay />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ portal/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      {/* <h1>Portal Career Discovery</h1> */}
+      <a href="/">Home</a>
+      <a href="/">Home</a>
+      <a href="/">Home</a>
+      <a href="/">Home</a>
+      <a href="/">Home</a>
+      {/* Add other navigation links as needed */}
+    </nav>
+  );
+};
+
+export default Navbar;
